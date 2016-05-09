774d670c538dd566c5558b80ea005524706cd047
HaroldYuan <1413349111@qq.com>
2016-05-09 14:24:18 +0800

    update

--- Classes/Drop.cpp
+++ Classes/Drop.cpp
@@ -5,24 +5,17 @@ bool Drop::init(){
 	if (!Sprite::init()){
 		return false;
 	}
-	auto dropPath_bullet = PATH_DROP_ADDBULLET_PICTURE;
-	auto spriteFrame_bullet = SpriteFrameCache::getInstance()->getSpriteFrameByName(dropPath_bullet);
-
-	auto dropPath_life = PATH_DROP_ADDBULLET_PICTURE;
-	auto spriteFrame_life = SpriteFrameCache::getInstance()->getSpriteFrameByName(dropPath_life);
-	initWithSpriteFrame(spriteFrame_bullet);
 
 	auto body = PhysicsBody::createBox(getContentSize());
 	setPhysicsBody(body);
 	body->setCategoryBitmask(0x01);       //0001
 	body->setContactTestBitmask(0x03);    //0011
 	body->setCollisionBitmask(0x02);      //0010
-
 	setTag(Enemy);
 	return true;
 }
 
-void Drop::hideDrop(float dt){
+void Drop::hideDrop(){
 	stopAllActions();
 	getParent()->removeChild(this);
 }
